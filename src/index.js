7c07822517797d2e88cbd334eea935ac64a8d23d
afeiship <1290657123@qq.com>
2019-10-26 13:25:30 +0800

    add: sass support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "webkit-sass-reset/dist/webkit-sass-reset.scss";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
